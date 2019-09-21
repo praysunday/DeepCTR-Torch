2ce552f1dbe17b0964221178de40c491e1ea92aa
zengkai <277248199@qq.com>
2019-09-21 18:35:20 +0800

    fixed some parameter

--- deepctr_torch/models/ccpm.py
+++ deepctr_torch/models/ccpm.py
@@ -40,11 +40,11 @@ class CCPM(BaseModel):
     """
     def __init__(self, linear_feature_columns, dnn_feature_columns, embedding_size=8, conv_kernel_width=(6, 5), conv_filters=(4, 4),
          dnn_hidden_units=(256,), l2_reg_linear=1e-5, l2_reg_embedding=1e-5, l2_reg_dnn=0, dnn_dropout=0,
-         init_std=0.0001, seed=1024, task='binary'):
+         init_std=0.0001, seed=1024, task='binary',device='cpu',dnn_use_bn=False,dnn_activation=F.relu):
 
         super(CCPM, self).__init__(linear_feature_columns, dnn_feature_columns, embedding_size=8, conv_kernel_width=(6, 5), conv_filters=(4, 4),
          dnn_hidden_units=(256,), l2_reg_linear=1e-5, l2_reg_embedding=1e-5, l2_reg_dnn=0, dnn_dropout=0,
-         init_std=0.0001, seed=1024, task='binary')
+         init_std=0.0001, seed=1024, task='binary',device='cpu',dnn_use_bn=False,dnn_activation=F.relu)
 
         self.dnn_hidden_units = dnn_hidden_units
         self.dnn = DNN(self.compute_input_dim(dnn_feature_columns, embedding_size, ), dnn_hidden_units,
